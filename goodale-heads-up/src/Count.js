a8d69e90d418b9404b88efa65928e710feb7bcf3
Paige <paigemacgregor33@gmail.com>
2020-01-16 20:18:12 -0800

    start skeleton app setup, count rendering

---
+++ goodale-heads-up/src/Count.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <Fragment>
+      Number of spaces: 544
+    </Fragment>
+  );
+}
+
