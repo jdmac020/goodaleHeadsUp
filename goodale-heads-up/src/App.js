ecff90057ec1c5b24c101b2852d0a4f19abdcdb4
Paige <paigemacgregor33@gmail.com>
2020-02-13 19:12:12 -0800

    add hero section

--- goodale-heads-up/src/App.js
+++ goodale-heads-up/src/App.js
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 import Count from './Count';
 import NavBar from './NavBar';
 import Footer from './Footer';
+import Hero from './Hero';
 
 export default function App() {
   return (
     <Fragment>
       <NavBar />
+      <Hero />
       <Count />
       <Footer />
     </Fragment>
---
+++ goodale-heads-up/src/Hero.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import './assets/css/Hero.css';
+import { Navbar, Nav } from 'react-bootstrap';
+// import Icon from '@mdi/react'
+// import { mdiCarHatchback } from '@mdi/js'
+
+export default function NavBar() {
+  return (
+    <div className="hero overlay">
+      <h1>Goodale Heads Up</h1>
+    </div>
+  );
+}
+
