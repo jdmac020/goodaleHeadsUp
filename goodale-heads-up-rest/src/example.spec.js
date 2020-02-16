3532cfdd466fe0588ecc1f5d7d24426f040b7a2b
jdmac020 <john.macgregor@gmail.com>
2020-02-16 13:15:20 -0500

    Some example tests

---
+++ goodale-heads-up-rest/src/example.spec.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+
+describe('Example tests', () => {
+
+    it('Is True', () => {
+      expect(true).to.eql(true);
+    });
+
+    it('Is False', () => {
+      expect(false).to.eql(false);
+    });
+  });
