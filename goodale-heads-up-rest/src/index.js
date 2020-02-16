f779ec8ac81ea867aa2546ac1b6002e4ed68afda
jdmac020 <john.macgregor@gmail.com>
2020-02-16 14:57:53 -0500

    Add routing

--- goodale-heads-up-rest/src/index.js
+++ goodale-heads-up-rest/src/index.js
@@ -1,5 +1,9 @@
-import 'dotenv/config'
-import callMyFunction from './services/exampleService.js'
+import express from 'express';
+import 'dotenv/config';
+import callMyFunction from './services/exampleService.js';
+
+const app = express();
+const portNumber = 3001;
 
 console.log('I AM COUNTING SPACES HERE!\nLike nonstop. On a loop. For reasons.\n');
 
@@ -8,3 +12,13 @@ callMyFunction(() => {
 });
 
 console.log(process.env.MY_SECRET_FIELD);
+
+app.get('/', (req, res) => {
+    res.send('We are counting spaces SO HARD right now!');
+  });
+
+app.get('/test', (req, res) => {
+    res.send('We are unit testing literally everything, even getters and setters.');
+});
+
+app.listen(portNumber, () => console.log("Listening on port " + portNumber));
